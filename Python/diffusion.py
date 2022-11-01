718c4a8a251b18d9956abd3f7487f675ec6a92f6
McGregor M. Koenig <koenig_mm@cobra.cs.mercer.edu>
2022-11-01 11:21:59 -0400

    Python with Partition

--- Python/diffusion.py
+++ Python/diffusion.py
@@ -6,6 +6,12 @@ maxsizestr = input("Number of divisions: ")
 
 maxsize = int(maxsizestr)
 
+answer = input("With partition? (y/n) ")
+
+partition = answer == "y"
+
+
+
 
 cube = zeros((maxsize, maxsize, maxsize), dtype=float64)  #zeros the cube
 
@@ -26,48 +32,116 @@ cube[0][0][0] = 1.0e21
 time = 0.0 #keeps up with system time
 ratio = 0.0 # lets us know if the room is equilibrated yet
 
-while ratio < 0.99:
-	for i in range(0, maxsize):
-		for j in range(0, maxsize):
-			for k in range(0, maxsize):
-				for l in range(0, maxsize):
-					for m in range(0, maxsize):
-						for n in range(0, maxsize):
-							if( (i == l and j == m and k == n+1) or
-							(i == l and j == m and k == n-1) or
-							(i == l and j == m+1 and k == n) or
-							(i == l and j == m-1 and k == n) or
-							(i == l+1 and j == m and k == n) or
-							(i == l-1 and j == m and k == n) ):
-								change = (cube[i][j][k] - cube[l][m][n]) * DTerm
-								cube[i][j][k] = cube[i][j][k] - change
-								cube[l][m][n] = cube[l][m][n] + change
+if not partition:
+	while ratio < 0.99:
+		for i in range(0, maxsize):
+			for j in range(0, maxsize):
+				for k in range(0, maxsize):
+					for l in range(0, maxsize):
+						for m in range(0, maxsize):
+							for n in range(0, maxsize):
+								if( (i == l and j == m and k == n+1) or
+								(i == l and j == m and k == n-1) or
+								(i == l and j == m+1 and k == n) or
+								(i == l and j == m-1 and k == n) or
+								(i == l+1 and j == m and k == n) or
+								(i == l-1 and j == m and k == n) ):
+									change = (cube[i][j][k] - cube[l][m][n]) * DTerm
+									cube[i][j][k] = cube[i][j][k] - change
+									cube[l][m][n] = cube[l][m][n] + change
 
-	time = time + timestep
+		time = time + timestep
 
-	sumval = 0.0
-	maxval = cube[0][0][0]
-	minval = cube[0][0][0]
+		sumval = 0.0
+		maxval = cube[0][0][0]
+		minval = cube[0][0][0]
 
-	for i in range(0, maxsize):
-		for j in range(0, maxsize):
-			for k in range(0, maxsize):
-				maxval = max(cube[i][j][k], maxval)
-				minval = min(cube[i][j][k], minval)
-				sumval = sumval + cube[i][j][k]
+		for i in range(0, maxsize):
+			for j in range(0, maxsize):
+				for k in range(0, maxsize):
+					maxval = max(cube[i][j][k], maxval)
+					minval = min(cube[i][j][k], minval)
+					sumval = sumval + cube[i][j][k]
 
-	ratio = minval/maxval
+		ratio = minval/maxval
 
 
-	print(ratio)
-	print(time,  " " , cube[0][0][0])
-	print( " " , cube[maxsize-1][0][0])
-	print(        " " , cube[maxsize-1][maxsize-1][0] );
-	print(        " " , cube[maxsize-1][maxsize-1][maxsize-1] );
-	print(        " " , sumval , "\n");
+		print(ratio)
+		print(time,  " " , cube[0][0][0])
+		print( " " , cube[maxsize-1][0][0])
+		print(        " " , cube[maxsize-1][maxsize-1][0] )
+		print(        " " , cube[maxsize-1][maxsize-1][maxsize-1] )
+		print(        " " , sumval , "\n")
+
+#case if partition
+else:
+
+	#creates and oneses the bitmap
+	bitmap = ones((maxsize, maxsize, maxsize), dtype=float64)
+
+	#zero the partition of the bitmap
+	for i in range(0,maxsize):
+		for j in range(0,maxsize):
+			for k in range(0,maxsize):
+				if i == int(maxsize/2) and j >= 1 + (maxsize * .25):
+					bitmap[i,j,k] = 0
+				else:
+					bitmap[i,j,k] = 1
+
+
+
+
+
+
+	while ratio < 0.99:
+		for i in range(0, maxsize):
+			for j in range(0, maxsize):
+				for k in range(0, maxsize):
+					for l in range(0, maxsize):
+						for m in range(0, maxsize):
+							for n in range(0, maxsize):
+								if bitmap[i,j,k] == 1 and bitmap[l,m,n] == 1:
+
+									if( (i == l and j == m and k == n+1) or
+									(i == l and j == m and k == n-1) or
+									(i == l and j == m+1 and k == n) or
+									(i == l and j == m-1 and k == n) or
+									(i == l+1 and j == m and k == n) or
+									(i == l-1 and j == m and k == n) ):
+										change = (cube[i][j][k] - cube[l][m][n]) * DTerm
+										cube[i][j][k] = cube[i][j][k] - change
+										cube[l][m][n] = cube[l][m][n] + change
+
+		time = time + timestep
+
+		sumval = 0.0
+		maxval = cube[0][0][0]
+		minval = cube[0][0][0]
+
+		for i in range(0, maxsize):
+			for j in range(0, maxsize):
+				for k in range(0, maxsize):
+					if bitmap[i,j,k] == 1:
+						maxval = max(cube[i][j][k], maxval)
+						minval = min(cube[i][j][k], minval)
+						sumval = sumval + cube[i][j][k]
+
+		ratio = minval/maxval
+
+
+		print(ratio)
+		print(time,  " " , cube[0][0][0])
+		print( " " , cube[maxsize-1][0][0])
+		print(        " " , cube[maxsize-1][maxsize-1][0] )
+		print(        " " , cube[maxsize-1][maxsize-1][maxsize-1] )
+		print(        " " , sumval , "\n")
+
+
+
+
+
+
 
 
 print("Box equilibrated in " , time , " seconds of simulated time.")
-
-
 
