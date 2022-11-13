cb1625930b5f4a7bd3c2262aed7fa4f56c1570cf
Your Name <koenig_mm@cobra.cs.mercer.edu>
2022-11-13 13:16:04 -0500

    Fortran with Partition

--- Fortran/diffusion.f95
+++ Fortran/diffusion.f95
@@ -5,6 +5,13 @@ PROGRAM DIFFUSION
         integer :: maxsize
         
         real, dimension(:,:,:), allocatable:: cube
+        real, dimension(:,:,:), allocatable:: bitmap
+
+
+        character(len = 1) :: answer
+
+        logical :: partition
+
 
 
         integer :: i, j, k, l, m, n     ! counter variables
@@ -46,6 +53,15 @@ ratio = 0.0
 write (*,*) "Number of divisions: "
 read(*,*) maxsize
 
+
+write(*,*) "With partiton? (y/n) "
+read(*,*) answer
+
+if (answer == 'y') then
+        partition = .true.
+        write(*,*) partition
+end if
+
 !calculate values that use maxsize
 timestep = (room_dimension / speed_molecules) 
 
@@ -68,15 +84,6 @@ allocate (cube (maxsize, maxsize, maxsize))
 
 cube = 0.0
 
-!do i = 1, maxsize
- !       do j = 1, maxsize
-  !              do k = 1, maxsize
-   !                     cube(i,j,k) = 0
-
-    !            end do
-     !   end do
-!end do
-
 
 !initialize first cell
 cube(1,1,1) = 1.0E21
@@ -84,45 +91,46 @@ cube(1,1,1) = 1.0E21
 change = 0.0
 
 
-do while (ratio < 0.99)
-
-        do i = 1, maxsize
-                do j = 1, maxsize
-                        do k = 1, maxsize
-                                do l = 1, maxsize
-                                        do m = 1, maxsize
-                                                do n = 1, maxsize
-                                                if(((i == l) .and. (j == m) .and. (k == n+1)) &
-                                                 .or. ((i == l) .and. (j == m) .and.(k == n-1)) &
-                                                .or. ((i == l) .and. (j == m+1) .and.(k == n)) &
-                                                .or. ((i == l) .and. (j == m-1) .and.(k == n)) &
-                                                .or. ((i == l+1) .and. (j == m) .and.(k == n)) &
-                                                .or. ((i == l-1) .and. (j == m) .and.(k == n))) then
-
-
-
-                                                        change = (cube(k, j, i) - cube(n, m, l)) * DTerm
-
-                                                        cube(k,j,i) = cube(k,j,i) - change
-                                                        cube(n,m,l) = cube(n,m,l) + change
+if (.not. partition) then
+        do while (ratio < 0.99)
+        
+                do i = 1, maxsize
+                        do j = 1, maxsize
+                                do k = 1, maxsize
+                                        do l = 1, maxsize
+                                                do m = 1, maxsize
+                                                        do n = 1, maxsize
+                                                                if(((i == l) .and. (j == m) .and. (k == n+1)) &
+                                                                .or. ((i == l) .and. (j == m) .and.(k == n-1)) &
+                                                                .or. ((i == l) .and. (j == m+1) .and.(k == n)) &
+                                                                .or. ((i == l) .and. (j == m-1) .and.(k == n)) &
+                                                                .or. ((i == l+1) .and. (j == m) .and.(k == n)) &
+                                                                .or. ((i == l-1) .and. (j == m) .and.(k == n))) then
 
 
 
-                                                end if
+                                                                change = (cube(k, j, i) - cube(n, m, l)) * DTerm
+
+                                                                cube(k,j,i) = cube(k,j,i) - change
+                                                                cube(n,m,l) = cube(n,m,l) + change
+
+
+
+                                                                end if
+                                                        end do
                                                 end do
                                         end do
                                 end do
                         end do
                 end do
-        end do
 
-        time = time + timestep
+                time = time + timestep
 
 
-        sumval = sum(cube)
-        maxx = maxval(cube)
+                sumval = sum(cube)
+                maxx = maxval(cube)
         !cube(1,1,1)
-        minn = minval(cube)
+                minn = minval(cube)
         !cube(1,1,1)
        
 
@@ -141,21 +149,140 @@ do while (ratio < 0.99)
       !    end do
 
 
-        ratio = minn/maxx
+                ratio = minn/maxx
 
 
-        print*, ratio
+                print*, ratio
 
-        print*, "Time: ", time
+                print*, "Time: ", time
        
-        print*, " ", cube(1,1,1),  " ", cube(maxsize, 1, 1)
-        print*,  " ", cube(maxsize, maxsize, 1)
-        print*, " ", cube(maxsize, maxsize, maxsize)
+                print*, " ", cube(1,1,1),  " ", cube(maxsize, 1, 1)
+                print*,  " ", cube(maxsize, maxsize, 1)
+                print*, " ", cube(maxsize, maxsize, maxsize)
 
-        print*, sumval
+                print*, sumval
 
 
-end do
+        end do
+
+else 
+
+        !allocate bitmap
+        allocate (bitmap (maxsize + 2, maxsize + 2, maxsize + 2))
+
+
+        !fill bitmap with ones
+
+        bitmap = 1.0
+
+
+
+        !zero the walls and partition of bitmap
+        do i = 1, maxsize + 2
+                do j = 1, maxsize + 2
+                        do k = 1, maxsize + 2
+
+                        if(i == 1 .or.j == 1 .or. k == 1) then
+                                bitmap(k,j,i) = 0
+                        end if
+                        if (i == maxsize + 2 .or. j == maxsize + 2 .or. &
+                                k == maxsize + 2) then
+                                bitmap(k,j,i) = 0
+                        end if
+                        if (i == ANINT((real(maxsize) + 2)/2) .and. j >= &
+ceiling((maxsize + 2) * .25 + 1)) then
+                                bitmap(k,j,i) = 0
+
+                        end if
+                        end do
+                end do
+        end do
+        
+        
+
+        do while (ratio < 0.99)
+        
+                do i = 1, maxsize
+                        do j = 1, maxsize
+                                do k = 1, maxsize
+                                        do l = 1, maxsize
+                                                do m = 1, maxsize
+                                                        do n = 1, maxsize
+                                                                if(bitmap(k + 1,j + 1,i + 1) &
+== 1 .and. bitmap(n + 1, m + 1, l + 1) == 1) then
+                                                                if(((i == l) .and. (j == m) .and. (k == n+1)) &
+                                                                .or. ((i == l) .and. (j == m) .and.(k == n-1)) &
+                                                                .or. ((i == l) .and. (j == m+1) .and.(k == n)) &
+                                                                .or. ((i == l) .and. (j == m-1) .and.(k == n)) &
+                                                                .or. ((i == l+1) .and. (j == m) .and.(k == n)) &
+                                                                .or. ((i == l-1) .and. (j == m) .and.(k == n))) then
+
+
+
+                                                                change = (cube(k, j, i) - cube(n, m, l)) * DTerm
+
+                                                                cube(k,j,i) = cube(k,j,i) - change
+                                                                cube(n,m,l) = cube(n,m,l) + change
+
+
+
+                                                                end if
+                                                                end if
+                                                        end do
+                                                end do
+                                        end do
+                                end do
+                        end do
+                end do
+
+                time = time + timestep
+
+
+        !        sumval = sum(cube)
+      !          maxx = maxval(cube)
+        !cube(1,1,1)
+       !         minn = minval(cube)
+        !cube(1,1,1)
+       
+
+                maxx = cube(1,1,1)
+                minn = cube(1,1,1)
+
+
+ 
+          do i = 1, maxsize
+                do j = 1, maxsize
+                        do k = 1, maxsize
+
+                                 if(bitmap(k + 1,j + 1,i + 1) ==1) then
+                                maxx = max(cube(k,j,i), maxx)
+                                minn = min(cube(k,j,i), minn)
+
+                                sumval = sumval + cube(k,j,i)
+                                end if
+                        end do
+                end do
+          end do
+
+
+
+
+                ratio = minn/maxx
+
+
+                print*, "Ratio: ", ratio
+
+                print*, "Time: ", time
+       
+                print*, " ", cube(1,1,1),  " ", cube(maxsize, 1, 1)
+                print*,  " ", cube(maxsize, maxsize, 1)
+                print*, " ", cube(maxsize, maxsize, maxsize)
+
+                print*, sumval
+
+        end do
+end if
+
 
         print*,  "Box equilibrated in ", time, " seconds of simulated time."
 
