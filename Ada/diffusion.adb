ee14accd96cf61cc7c1e5690f348fd9d7ec9bccf
Your Name <koenig_mm@cobra.cs.mercer.edu>
2022-11-13 13:13:14 -0500

    Ada with Partition

--- Ada/diffusion.adb
+++ Ada/diffusion.adb
@@ -8,6 +8,13 @@ procedure Diffusion is
 	maxsize : Integer;
 	
 	type cubee is array(Natural range <>, Natural range <>, Natural range <>) of Float;
+	type cubeee is array(Natural range <>, Natural range <>, Natural range <>) of Integer;
+
+
+	partition : Boolean := FALSE;
+	answer : STRING(1..1);
+
+
 
 	diffusion_coef : Float := 0.175;
 	room_dimension : Float := 5.0; -- five meters long room
@@ -35,31 +42,39 @@ begin
 	Put("Number of Divisions: ");
 	Get(maxsize);
 
+	--take partition as input
+	Put("With partition? (y/n) ");
+	Get(answer);
+
+	if answer = "y" then
+		partition := TRUE;
+	end if;
+
+
+
 	timestep := (room_dimension/speed_molecules)/Float(maxsize);
 	distance_bw := room_dimension/Float(maxsize);
 
 	DTerm := (diffusion_coef * timestep) / (distance_bw * distance_bw);
 
 
-	maxsize := maxsize - 1;
-
-
-	-- declareas and zeroes the cube
+	-- declares and zeroes the cube
 	declare
-	Cube : cubee(0..maxsize, 0..maxsize, 0..maxsize) := (Others =>(Others =>(Others => 0.0)));
+        Cube : cubee(0..maxsize-1, 0..maxsize-1, 0..maxsize-1) := (Others =>(Others =>(Others => 0.0)));
 	begin
 
 	Cube(0,0,0) := 1.0e21;
 
 
-	while(ratio < 0.99) loop
+	if partition /= TRUE then
+		while(ratio < 0.99) loop
 	
-		for I in 0..maxsize loop
-			for J in 0..maxsize loop
-				for K in 0..maxsize loop
-					for L in 0..maxsize loop
-						for M in 0..maxsize loop
-							for N in 0..maxsize loop
+		for I in 0..maxsize-1 loop
+			for J in 0..maxsize-1 loop
+				for K in 0..maxsize-1 loop
+					for L in 0..maxsize-1 loop
+						for M in 0..maxsize-1 loop
+							for N in 0..maxsize-1 loop
 								if (	((I = L) and (J = M) and (K = N+1)) or
 									(I = L and J = M and K = N-1) or
 									(I = L and J = M+1 and K = N) or
@@ -88,9 +103,11 @@ begin
 		maxval := Cube(0,0,0);
 		minval := Cube(0,0,0);
 
-		for I in 0..maxsize loop
-			for J in 0..maxsize loop
-				for K in 0..maxsize loop
+
+		-- find min and max of the cube
+		for I in 0..maxsize-1 loop
+			for J in 0..maxsize-1 loop
+				for K in 0..maxsize-1 loop
 					maxval := Float'Max(Cube(I,J,K), maxval);
 					minval := Float'Min(Cube(I,J,K), minval);
 
@@ -105,8 +122,7 @@ begin
 		ratio := minval/maxval;
 
 
-		Put("ratio: ");
-		Put_Line(Float'Image(ratio));
+		Put_Line("ratio: " & Float'Image(ratio));
 
 		Put_Line("");
 
@@ -130,6 +146,123 @@ begin
 
 
 	end loop;
+
+
+	else 
+
+
+		-- declare bitmap and fill with ones		
+		declare
+                Bitmap : cubeee(0..maxsize + 1, 0..maxsize + 1, 0..maxsize + 1) := (Others =>(Others =>(Others => 1)));
+                begin
+
+                -- zero walls and partition of bitmap
+		for I in 0..maxsize + 1 loop
+			for J in 0..maxsize + 1 loop
+				for K in 0..maxsize + 1 loop
+               
+					if(I = 0 or J = 0 or K = 0) then
+						Bitmap(I,J,K) := 0;
+                
+					elsif(I = maxsize + 1 or J = maxsize + 1 or K = maxsize + 1) then
+						Bitmap(I,J,K) := 0;
+                
+					elsif(I = Integer(Float'Floor((Float(maxsize) + 2.0)/2.0)) and Float(j) >= Float'Floor((Float(maxsize) + 2.0) * 0.25 + Float(1.0))) then
+						Bitmap(I,J,K) := 0;
+					end if;
+				end loop;
+			end loop;
+		end loop;
+
+
+
+
+
+		while(ratio < 0.99) loop
+	
+			for I in 0..maxsize-1 loop
+				for J in 0..maxsize-1 loop
+					for K in 0..maxsize-1 loop
+						for L in 0..maxsize-1 loop
+							for M in 0..maxsize-1 loop
+								for N in 0..maxsize-1 loop
+									if(Bitmap(I+1,J+1,K+1) = 1 and Bitmap(L+1,M+1,N+1) = 1) then
+										if (	((I = L) and (J = M) and (K = N+1)) or
+											(I = L and J = M and K = N-1) or
+											(I = L and J = M+1 and K = N) or
+											(I = L and J = M-1 and K = N) or
+											(I = L+1 and J = M and K = N) or
+											(I = L-1 and J = M and K = N) ) then
+											change := (Cube(I,J,K) - Cube(L,M,N)) * Dterm;
+						
+									
+											Cube(I,J,K) := Cube(I,J,K) - change;
+											Cube(L,M,N) := Cube(L,M,N) + change;
+
+
+
+										end if;
+									end if;
+								end loop;
+							end loop;
+						end loop;
+					end loop;
+				end loop;
+			end loop;
+
+			time := time + timestep;
+		
+			sumval := 0.0;
+			maxval := Cube(0,0,0);
+			minval := Cube(0,0,0);
+
+			for I in 0..maxsize-1 loop
+				for J in 0..maxsize-1 loop
+					for K in 0..maxsize-1 loop
+						if(Bitmap(I+1,J+1,K+1) = 1) then
+							maxval := Float'Max(Cube(I,J,K), maxval);
+							minval := Float'Min(Cube(I,J,K), minval);
+						
+							sumval := sumval + Cube(I,J,K);
+
+						end if;
+					end loop;
+				end loop;
+			end loop;
+
+
+			ratio := minval/maxval;
+
+
+			Put_Line("ratio: " & Float'Image(ratio));
+
+			Put_Line("");
+
+			Put(" time = ");
+			Put(Float'Image(time));
+
+			Put(" ");
+			Put(Float'Image(Cube(0,0,0)));
+			Put(" ");
+                	Put(Float'Image(Cube(maxsize - 1 ,0,0)));
+                	Put(" ");
+                	Put(Float'Image(Cube(maxsize - 1, maxsize - 1,0)));
+                	Put(" ");
+                	Put(Float'Image(Cube(maxsize - 1, maxsize - 1, maxsize - 1)));
+                	Put(" ");
+        	        Put(Float'Image(sumval));
+
+
+
+
+
+
+		end loop;
+
+
+
+	end;
+	end if;
 
 	Put("Box equilibrated in ");
 	Put(Float'Image(time));
